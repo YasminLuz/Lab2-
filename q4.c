d45a1ef7812377c9f20e7d2dab899e25042ce893
YasminLuz <31142485+YasminLuz@users.noreply.github.com>
2017-11-29 03:06:51 -0300

    Update q4.c

--- q4.c
+++ q4.c
@@ -11,23 +11,22 @@ Programa que o usuário informa um texto de até 250 caracteres e programa mos
 
 
 void main(){
-	char palavra[LIMITE];
+	char texto[LIMITE];
 	char letra; 
 	int x = 0,y = 0, cont;
 	
-	printf("Palavra: ");
-	fgets(palavra, 250, stdin);
+	printf("Texto: ");
+	fgets(texto, 250, stdin);
 	//fflush(stdin);
 	printf("\nPesquisar pela letra: ");
 	scanf(" %c", &letra);
 	
 	for(x;x < LIMITE;x++){
-		//for(y = x; y < LIMITE; y++){
-			
-			if(letra == palavra[x])
+	   
+		if (letra == texto[x])
 				cont++;
 			
-		//}
+		
 	}
 	
 	printf("A letra %c aparece %d vezes", letra,cont-1);
