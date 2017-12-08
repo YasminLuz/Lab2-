9fc2114e77a6abb04c843cbb786e41dbcc9076e5
YasminLuz <31142485+YasminLuz@users.noreply.github.com>
2017-12-08 20:19:24 -0300

    Create horaedatasistema.c
    Hora e data do sistema (Windows)

---
+++ horaedatasistema.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+main(){
+	int x, op, n1,n2;
+	
+	do{
+		printf("######## MENU ########\n \n");
+		printf("1 - Data do sistema \n"
+		       "2 - Hora do sistema \n"
+           "3 - Somar dois numeros\n"
+			     "4 - Sair \n");
+	  scanf("%d", &op);
+	    
+	    switch(op){
+	      	case 1: system ("date"); break;
+	        
+	        case 2: system ("time"); break;
+	        	
+	        case 3: {
+			        printf("Digite dois numeros: ");
+	                scanf("%d %d",&n1, &n2);
+	                printf("%d \n", n1+n2);
+	                break;
+	               }
+	                
+	        	
+			    case 4: printf("Ate a proxima"); break;
+          
+          default: printf("A opcao digitada nao existe");
+     }
+		
+	} while(op != 4);
+	
+	
+}
