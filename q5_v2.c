62c0c5d5ced38256c46e84933a72edd629c81094
Yasmin <31142485+YasminLuz@users.noreply.github.com>
2017-12-19 01:19:31 -0300

    Update q5_v2.c

--- q5_v2.c
+++ q5_v2.c
@@ -4,32 +4,22 @@
 //Usando int para inverter numero
 
 main(){
-	int x = 0, tam, num;
-	int resto = 0, div = 0, quoc = 10000;
-	int armazena[TAM];
+	int x = 0, num;
+	int armazena[TAM]= {};
     
     
     printf("Numero:\n");
-    scanf(" %d",&num);
- 
-    //printf("%d",sizeof(num));
-   
-    div = num /quoc;
-    resto = div%quoc;
-    for(x; x < TAM; x++){
-    	
-    	armazena[x]= div;
-    	div = resto%quoc;
-    	resto = div %quoc;
-    	quoc= quoc/10;
-    	div= resto/quoc;
-	}
+    scanf("%d",&num);
+
+    for(x;x < TAM;x++){  	
+    	armazena[x]= num%10;
+    	num = num/10;
+    }
 	
 	
-	for(x = TAM; x >=0; x--){
-		if (armazena[x] != ' ')
-		  printf("%d", armazena[x]);
-	}
+    for(x = 0; x  < TAM; x++)
+		printf("%d", armazena[x]);
+    
    
     
 }
