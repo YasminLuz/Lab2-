af2dd2e75e63c7bf30166fe2003821bbb2decde9
Yasmin <31142485+YasminLuz@users.noreply.github.com>
2018-01-28 15:40:09 -0300

    Update Q2  id.c

--- Q2  id.c
+++ Q2  id.c
@@ -7,7 +7,7 @@ typedef struct{
 }dma;
 
 
-void confere(dma data);
+int confere(dma data);
 int entrada(int ano);
 int maiordata(dma data, dma data2);
 void calculaid(dma data, dma data2);
@@ -15,8 +15,6 @@ int bissexto(int ano);
 int fevereiro(dma data, int status);
 int mes30(dma data);
 int mes31(dma data);
-
-
 
 
 int main(void){
@@ -31,80 +29,89 @@ int main(void){
 	printf("Data inicial(dd/mm/aaaa): \t");
 	gets(d1);
 	
-	sscanf(d1,"%d/%d/%d", &data.dia, &data.mes, &data.ano);
+	sscanf(d1,"%d/%d/%d", &data.dia, &data.mes, &data.ano); 
       	 
-      	 
-	//Analisando data 1
-	confere(data);
-	entrada(data.ano);
-	bissexto(data.ano); 
-    fevereiro(data, bissexto(data.ano));
-    mes30(data);
-    mes31(data);
-      	 
+		//Analisando data 1
+		confere(data);
+		entrada(data.ano);
+		bissexto(data.ano); 
+	    fevereiro(data, bissexto(data.ano));
+	    mes30(data);
+	    mes31(data);
+
       	 
 	//Entrada data 2	
 	printf("\nData final(dd/mm/aaaa): \t");
-	//scanf(" %d/%d/%d",&data2.dia,&data2.mes,&data2.ano);
 	gets(d2);
 	
 	sscanf(d2,"%d/%d/%d", &data2.dia, &data2.mes, &data2.ano);
-    
-    //Analisando data 2
-	confere(data2);
-	entrada(data2.ano);
-	bissexto(data2.ano); //verificação de ano bissexto ou não bissexto
-    fevereiro(data2, bissexto(data2.ano));
-    mes30(data2);
-    mes31(data2);
+	
+	    //Analisando data 2
+		confere(data2);
+		entrada(data2.ano);
+		bissexto(data2.ano); //verificação de ano bissexto ou não bissexto
+	    fevereiro(data2, bissexto(data2.ano));
+	    mes30(data2);
+	    mes31(data2);
       	 
-//	if (entrada(data.ano)!=0 && entrada(data2.ano)!=0)	    
-		switch (maiordata(data, data2) ){
+	if (confere(data)!=0 && confere(data2)!=0){
+		switch (maiordata(data, data2)){
 		
     		case 1: calculaid(data, data2);break;
-    		case 0: printf("Digite o ano 1 menor que o ano 2"); break;
-      		default:printf("algo de errado nao esta certo");break;
-	}
+    		case 0: printf("Digite o ano 1 menor que o ano 2\n"); break;
+      		default:printf("algo de errado nao esta certo\n");break;
+		}
+	}else
+	  printf("Digite as duas datas de forma valida");
 	
-	system("pause");
+	//system("pause");
 	
   return 0;    
 }
 
 
-void confere(dma data){
+int confere(dma data){
+	int ok = 0;
 		
 	if(data.dia > 31 || data.dia < 1){
-	   if(data.mes > 12 || data.mes < 1){
-		printf("Digite um dia valido\n");		   
-		printf("Digite um mes valido \n");	
-	   }
+		printf("Digite um dia valido\n");
+		ok = 1;
+	}		   
+			
+	
+	if(data.mes > 12 || data.mes < 1){
+		printf("Digite um mes valido \n");
+		ok = 1;
 	}
 		
-	if(entrada(data.ano)==0)
-		printf("Digite um ano valido \n");	     				  
+	if(entrada(data.ano)==0){
+		printf("Digite um ano valido \n");
+		ok = 1;
+	}
+		
+  return ok;			     				  
 }
 
 #define TAM 4
 int entrada(int ano){//verifica digitos de entrada do ano é valido
 	
-	int x, i, date, anodig[TAM]= { }, resto[TAM] = { }, cont=0;
+	int x, i, date, anodig[TAM]= { }, resto[TAM] = { }, cont ;
 	
 	
 	date = ano;
 		
-	if (ano>999 && ano<=9999){ //se ano possui 4 digitos
+	if (ano > 999 && ano <= 9999){ //se ano possui 4 digitos
 		
-	  for(x= 1000, i=0;(x > 0) && (i < 4); x/=10, i++){
+	  for(x = 1000, i = 0;(x > 0) && (i < 4); x /= 10, i++){
 		anodig[i] = date/x;
 		resto[i] = date%x;
 		date = resto[i];
 		cont++;
       }
       
-    }else if(ano<=99 && ano>9){ //se ano possui 2 digitos
+    }else if(ano <= 99 && ano > 9){ //se ano possui 2 digitos
     	
-       for(x= 10, i=0;(x > 0) && (i < 2); x/=10, i++){
+       for(x = 10, i = 0;(x > 0) && (i < 2); x /= 10, i++){
 		anodig[i] = date/x;
 		resto[i] = date%x;
 		date = resto[i];
@@ -112,7 +119,7 @@ int entrada(int ano){//verifica digitos de entrada do ano é valido
        }	
       
 	}else{
-	    cont=0;
+	    cont = 0;
     }
     
 	return cont;
@@ -133,64 +140,69 @@ int maiordata(dma data, dma data2){ //analisa as possibilidades do ano1 ser meno
 
 void calculaid(dma data, dma data2){//calcula idade do individuo
 	
-	int anos = 0, meses = 0, dias = 0 , qntdias[11]={31,28,31,30,31,30,31,31,30,31,30,31};
-	//diasextra = 0, ano[TAM-1, x = 0,
-//		
-//	if (entrada(data2.ano)== 2)
-//		for(;x < TAM ;x++)
-//		 if(x<2)
-//		 	ano[x]=0;
-//		 else{
-//		 	int date = data2.ano;
-//		    ano[x]=date%10;
-//		    date=data2.ano/10;
-//		}
-//		
-//	for(x=0; x<TAM; x++)	
-//		anos= ano[x]-data.ano;
+	int anos = 0, meses = 0, dias = 0, qntdias[]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int x, y = 0, diasextras = 0, cont = 0;
 
 
 	anos = data2.ano-data.ano;
-    meses = data2.mes-data.mes;
+       meses = data2.mes-data.mes;
 	
 	if(meses<0){
 		anos--;
 		meses+=12;
-	}	
+	}
 	
-	dias = data2.dia-data.dia;
+	//contador de bissextos nas diferenças
+	for (x = 1; x < anos; x++){
+		if(bissexto(data2.ano-x) == 1){
+			diasextras++;
+		}
+	}
+	printf("EXTRAS %d\n", diasextras);
+
 	
+	//conta meses
+	y = data.mes;
+	for(x = 0; x<= 11; x++){
+		y++;
+		
+		if(y==12 && (y!=data2.mes)){
+			y = 0;
+			
+		}else if(y==data2.mes)
+			break;
+		
+		cont++;
+	}
+	printf("CONT %d\n", cont);
+	
+	//conta dias
+	dias=(data2.dia-data.dia) + diasextras;
+	
+	printf("DIAS %d\n", dias);
+	
+	printf("DATA 2 MES %d\n", data2.mes);
+
 	if(dias<0){
 	  meses--;
 	  	if(meses<0){
 		  anos--; 
 		  meses+=12;
 	    }
+	    
+	    if(bissexto(data2.ano)==1)
+	    	dias+=29;
+	    else 
+	  		dias+=qntdias[data2.mes-1];//menos 1 pq o indice começa de 0
 	}
-	dias+=qntdias[meses];
-    
-	 
-	  
-//	for(x= data.ano; x < data2.ano; x++){
-//		if (bissexto(x) == 1){
-//			diasextra++;
-//		}else
-//			continue;	
-//    }
-    
-   // dias =  (365*anos)+ diasextra;
-    
-    //meses= data2.mes-data.mes;
-		
-	//dias= (366*cont)+(365*(anos-cont));
+	printf("QNT DIAS %d\n", qntdias[data2.mes+1]);
 	
-	//dias= (cont*);
-	
-//	
-//	if ((anos == 0) && (meses == 0) && (dias == 0))
-//		printf("Voce digitou a data atual! \n");
-//	else
-	printf("\n A pessoa tem %d ano(s),%d mes(es) e %d dia(s)\n\n", anos, meses, dias);
+    
+
+	if ((anos == 0) && (meses == 0) && (dias == 0))
+		printf("Voce digitou a mesma data! \n");
+	else
+	printf("\nA pessoa tem %d ano(s),%d mes(es) e %d dia(s)\n\n", anos, meses, dias);
 	
 }
 
@@ -218,7 +230,7 @@ int fevereiro(dma data, int status){
 		   		 if(data.dia > 0 && data.dia < 29)
        				printf("A data %d/%d/%d e valida \n",data.dia, data.mes, data.ano);
 			     else if (data.dia == 29) 
-			    	printf("Ano bissexto. \n A data %d/%d/%d e valida \n\n",data.dia, data.mes, data.ano);	
+			    	printf("Ano bissexto. \nA data %d/%d/%d e valida \n\n",data.dia, data.mes, data.ano);	
 				 else
                  	printf("O dia nao e correspondente ao mes\n");
 				
@@ -241,7 +253,8 @@ return status;
 
 
 int mes31(dma data){
-
+	
+  int ok = 0;
   switch(data.mes){
   	case 1: 
 	case 3: 
@@ -250,29 +263,32 @@ int mes31(dma data){
 	case 8: 
 	case 10: 
 	case 12:{  		
-    		  if (data.dia > 0 && data.dia <= 31)
+    		  if (data.dia > 0 && data.dia <= 31){
+			     ok = 1;
       			 printf("A data %d/%d/%d e valida \n", data.dia, data.mes, data.ano);
-   			  else 
+   			  }else 
        			printf("O dia nao e correspondente ao mes \n");
      		       		 
 			  break;
   			}
   }
   
-return 0;
+return ok;
 }
 
 
 int mes30(dma data){
-
+	
+  int ok = 0;
   switch(data.mes){
   	case 4:
 	case 6: 
 	case 9: 
 	case 11:{
-			  if (data.dia > 0 && data.dia <= 30)
-     	 		 printf("A data %d/%d/%d e valida \n", data.dia, data.mes, data.ano);
-     		  else 
+			  if (data.dia > 0 && data.dia <= 30){
+   			     int ok = 0;
+			  	 printf("A data %d/%d/%d e valida \n", data.dia, data.mes, data.ano);
+     		  }else 
      			 printf("O dia nao e  correspondente ao mes \n");
      		      		  
      		  break;
