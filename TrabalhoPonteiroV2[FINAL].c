3ff05db18e0cc7714969e8e5f6b8cd4ae19338be
Yasmin <31142485+YasminLuz@users.noreply.github.com>
2018-03-23 00:18:54 -0300

    Update and rename TrabalhoPonteiroV2[INCOMPLETO].c to TrabalhoPonteiroV2[FINAL].c

--- TrabalhoPonteiroV2[FINAL].c
+++ TrabalhoPonteiroV2[FINAL].c
@@ -24,24 +24,21 @@ int tamanho(no *vetor, int pos);
 void liberar(no *vetor);
 int inserir(no *vetor);
 void transformar(no *vetor, int x);
-void imprimirprincipal(no vetor[]);
-void ordenaraux(no *vetor);
-void ordenartodos(no *vetor);
+void imprimirprincipal(no *vetor);
+void selectionsort( int *vetn, int soma);
+void ordenaraux(no *vetor, int tam, int i);
+int ordenartodos(no *vetor,int *tvet, int *nvet, int soma, int i, int y);
 int aumentar(no *vetor);
-int excluir(no *vetor);
+int excluir(no *vetor, int pos,int exc);
 void limpar();
 
-
 int main(void){
-	
 	//CRIA LISTA PRINCIPAL
 	no vetor[TAM];//array de ponteiros com 10 posicoes fixas(lista estatica) para estruturas auxiliares
-
-	//vetor->qnt = -1;//diferente de NULL, nenhuma posicoes ocupadas no inicio
 	
 	inicializar(vetor);
 	   
-	int opc, opd, cont = 0 , posicao = 0, num; 
+	int opc, cont = 0 , posicao = 0; 
     
     do{
     	volta: opc = menu();
@@ -68,14 +65,12 @@ int main(void){
 										        if (vetor[posicao].qnt == -1){ //se não tiver estrutura auxiliar criada nessa posicao, faca
 										         
 										            vetor[posicao].qnt++;//auxiliar criada e igual a 0
-										            printf("%d",vetor[posicao].qnt);
 											        cont++; 
 											        
 										            //fflush(stdin);
 										         	printf("Tamanho estrutura: ");
 										         	scanf("%d", &vetor[posicao].capac);
 										         	
-										         	//printf("CAPACID%d",vetor[posicao].capac);
 										         	vetor[posicao].vet2 = (aux *) malloc(vetor[posicao].capac * sizeof(aux));
 										         	
 										            printf("Lista criada!\n");
@@ -99,17 +94,16 @@ int main(void){
 										
 										}
 									
-								case 2:{ if((vetor[posicao].qnt == vetor[posicao].capac)){
-												printf("Lista toda preenchida\n");
-												limpar();
-												goto volta;
-										 }
-										 
-								         inserir(&vetor[0]);
-									     limpar();
-										  goto volta;
+								case 2:{ 	inserir(&vetor[0]);
+											if((vetor[posicao].capac > 0)&&(vetor[posicao].qnt == vetor[posicao].capac)){
+													printf("Lista toda preenchida\n");
+													limpar();
+													goto volta;
+											}
+										    limpar();
+											goto volta;
 									     
-										break;
+										 break;
 										}
 									
 										
@@ -132,11 +126,48 @@ int main(void){
 											break;
 									       }
 									       
-	                                case 2:{
+	                                case 2:{int x;
+											for(x = 0; x < TAM; x++)
+												ordenaraux(vetor, vetor[x].qnt, x);
+											
 											break;
 									       }
 									
-									case 3:{ordenartodos(vetor);
+									case 3:{int x = 0, soma = 0,cont = 0, y = -1;
+											no ini[TAM];
+											
+											for(x = 0; x < TAM; x++){
+												if(vetor[x].qnt > 0)
+													soma += vetor[x].qnt;
+												else
+												  continue;	
+											}
+									        
+									        int *tvet = (int *) malloc(soma * sizeof(int));// vetozao
+									        int *nvet = (int*) malloc(soma * sizeof(int));
+									        
+									        for(x = 0; x < TAM; x++){
+									        
+									          for(y = 0; y < TAM; y++){	
+									          
+									        	    if(vetor[x].qnt > 0){									        		
+														ini[x].vet2 = (aux *) malloc(sizeof(aux)); //recebe estruturas aux
+														ini[x].vet2 = vetor[x].vet2;  
+																												
+														ordenartodos(ini, tvet,nvet, soma, x, y);
+													//	y =ordenartodos(ini, tvet,nvet, soma, x, y);
+														y+=vetor[x].qnt;
+												    }
+												
+											   }
+											   
+											   		if(vetor[x].qnt < 0)
+											    		cont++;//verifica se ja alcancou o final do laco
+									  		}  
+																								
+												 	if(cont == TAM)
+												   		printf("Nao tem elementos alocados nesse vetor\n");
+												   		
 											break;
 										   }
 									
@@ -155,12 +186,21 @@ int main(void){
 						
 							switch(opc){
 								
-									case 1:{ excluir(vetor);
+									case 1:{int pos, exc;
+											printf("\nPosicao principal para excluir[1-10]: ");
+											scanf("%i", &pos);
+											
+											pos--;
+											
+											printf("\nElemento a ser tirado: ");
+											scanf("%i", &exc);
+											
+											excluir(vetor, pos, exc);
 										
 											 break;
 									       }
 									       
-	                                case 2: goto volta; 
+	                                case 2: limpar(); goto volta; 
 	                                
 									default:printf("Digite uma operacao valida!\n"); 		
 							}
@@ -183,9 +223,7 @@ int main(void){
 			
 			default: printf("Digite uma operacao valida!\n");break;	
 		}
-		
-    //	limpar();
-    	
+	
 	}while(opc != 5);
     
     liberar(vetor);//para a lista sequencial
@@ -213,18 +251,17 @@ void inicializar(no *vetor){
 
 
 int tamanho(no *vetor, int pos){
-	
-	if ((pos>9) || (pos<0)){  //leitura para usuario e de 1 a 10  e de 0 a 9 programa
+	if ((pos > 9) || (pos < 0)){  //leitura para usuario e de 1 a 10  e de 0 a 9 programa
 		printf("\nDigite um numero dentro da capacidade maxima/minima do vetor\n");
 		return 0;
-	}
+	}else
+	return 1;
 }
 
 
 void liberar(no *vetor){
 	int x;
-	
-	for(x =0 ; x <TAM; x++)
+
 		free(vetor[x].vet2);
 }
 
@@ -234,32 +271,31 @@ int submenu(int i){
 	
 	switch (i){ //qual submenu exibir a partir das entradas
 		
-		case 1: {printf("\n  	 [1] Criar lista \n"
-			   "  	 [2] Inserir elemento \n"
-			   "   	 [3] Voltar menu principal\n ");
-		scanf("%i",&opc);
-		
-		break;
+		case 1: {	printf("\n  	 [1] Criar lista \n"
+				    "  	 [2] Inserir elemento \n"
+				    "   	 [3] Voltar menu principal\n ");
+					scanf("%i",&opc);
+			
+					break;
 				}
 				
-	case 2:{printf("\n 	[1] Todas as estruturas \n"
-			   "   	[2] Auxiliares ordenas \n"
-			   "   	[3] Todas as estruturas ordenadas\n"
-			   "  	[4] Voltar ao menu principal \n");
-		scanf("%i",&opc);
+		case 2:{	printf("\n 	[1] Todas as estruturas \n"
+			   		"   	[2] Auxiliares ordenadas \n"
+			   		"   	[3] Todas as estruturas ordenadas\n"
+			   		"  	[4] Voltar ao menu principal \n");
+					scanf("%i",&opc);
 		
-		break;
-	}
+					break;
+				}
 	
-	case 3:{
-			
-		printf("\n 	[1] Excluir elemento da lista auxiliar \n"
-			   "   	[2] Voltar ao menu principal\n");
-		scanf("%i",&opc);
+		case 3:{	printf("\n 	[1] Excluir elemento da lista auxiliar \n"
+			   		"   	[2] Voltar ao menu principal\n");
+					scanf("%i",&opc);
 		
-		break;
+					break;
+				}
 	}
-	}		
+			
 return opc;
 	
 }
@@ -282,21 +318,15 @@ int menu(void){
 
 
  int inserir(no *vetor){//insere na ultima posicao
- 	
  	int pos, val;
  	no tmp1[TAM], tmp[TAM];
- 	
-// 	no auxi[TAM];
- 	
+
    	voltar: printf("\nPosicao principal para inserir[1-10]: ");
 	scanf("%i", &pos);
+	fflush(stdin);
 	
 	pos--;
 	
-	printf("QUANT%d\n",vetor[pos].qnt);
-	printf("CAPAC%d\n",vetor[pos].capac);
-	
-	//tamanho(vetor, pos);
 	
 	if(tamanho(vetor, pos) == 0){
 		goto voltar;
@@ -307,14 +337,17 @@ int menu(void){
 		
 
 	   if(vetor[pos].qnt == 0){//se tiver estrutura criada e for primeira entrada
+ 		no nov[TAM];
+ 		nov[pos].vet2 = (aux *)malloc(sizeof(aux));
  		
-		vetor[pos].vet2->valor= val;
-		vetor[pos].vet2->prox = NULL;
+		nov[pos].vet2->valor= val;
+		nov[pos].vet2->prox = NULL;
+		vetor[pos].vet2 = nov[pos].vet2;
 		
 	    vetor[pos].qnt++;
 	    return 1;
 	    
-	} else if (vetor[pos].qnt > 0){ //se ja houver elementos
+	} else if (vetor[pos].qnt > 0){ //se ja houver elementos na estrutura aux
 
 		//inserir no inicio vetor tmp auxiliar
 		no tmp[TAM];
@@ -330,24 +363,12 @@ int menu(void){
         vetor[pos].vet2 = nnovo[pos].vet2;
         
 		vetor[pos].qnt++;//incrementa ate chegar no tamanho declarado para a estrutura aux
-
+    
 		return 1;
-		
-	   
-	    //teste
-        while (vetor[pos].vet2 != NULL) {
-         printf("     %d", vetor[pos].vet2->valor);
-	     vetor[pos].vet2 = vetor[pos].vet2->prox;
-		}
-        
-
-		vetor[pos].qnt++;//incrementa ate chegar no tamanho declarado para a estrutura aux
-
-	//	return 1;
 	
 	} else if (vetor[pos].qnt == -1){//se nao tiver aux criada nessa posicao, exiba a menssagem
 	
-		printf("Não existe estrutura criada nessa posicao \n");
+		printf("Nao existe estrutura criada nessa posicao \n");
 		return 0;
 	}
 
@@ -355,9 +376,8 @@ int menu(void){
  
  
  void transformar(no *vetor, int x){ //transforma lista em vetor e facilitar busca por conteudo, imprime
-    
 	int y = 0, tam = vetor[x].qnt;
-	//printf("QUANT %d\n", tam);
+	
 	no novo[TAM]; //vetor auxiliar para receber referencia do primeiro valor da estrutura
 	novo[x].vet2 = (aux *) malloc(tam * sizeof(aux)); //aloca espaço na posicao passada pelo parametro
 	novo[x].vet2 = vetor[x].vet2;  
@@ -366,24 +386,20 @@ int menu(void){
 	
 	     while (novo[x].vet2 != NULL) {
 	     	   tvet[y] = novo[x].vet2->valor;
-	     	   printf("|");
-	           printf(" %d |", tvet[y]);
-	           novo[x].vet2 = novo[x].vet2->prox;
+	           printf("     %d ", tvet[y]);
 	           y++;
+	           novo[x].vet2 = novo[x].vet2->prox;
 	     }
 	     printf("\n");
- 	
- 	liberar(novo);
  }
 
  
-void imprimirprincipal(no vetor[]){
-	int x, y;
-		
+void imprimirprincipal(no *vetor){
+	int x = 0, y = 0;
 		
 	for(x =0 ; x < TAM; x++){
 		
-		if(vetor[x].qnt < 1)
+		if(vetor[x].capac < 1)
 		   printf("Vetor[%d] = vazio\n", x+1);
 		else{
 		  printf("Vetor[%d] = %d \n ",x+1, vetor[x].capac);
@@ -429,147 +445,147 @@ int aumentar(no *vetor){
 }
 
 
-void selectionsort( int vetn[], int soma){
-	
-    int min, aux2, x, y;//variaveis da ordenacao
+void selectionsort( int *vetn, int soma){
+    int min = 0, aux2 = 0, x = 0, y = 0, z = 0;//variaveis da ordenacao
     
+    if(soma > 1){
     //ordenar selection sort
-    for(x = 0; x < (soma - 1); x++){
-    	min = x; //considera primeiro numero como menor numa lista nao ordenada
-    	
-    	for(y = x + 1; y < soma; y++){
-    		if(vetn[y] < vetn[min])
-    		   min = y; //troca
+	    for(x = 0; x < (soma - 1); x++){
+	    	min = x; //considera primeiro numero como menor numa lista nao ordenada
+	    	
+	    	for(y = x + 1; y < soma; y++){
+	    		if(vetn[y] < vetn[min])
+	    		   min = y; //troca
+			}
+			
+			if(x != min){
+			   aux2 = vetn[x];
+			   vetn[x] = vetn[min];
+			   vetn[min] = aux2;	
+			}
 		}
 		
-		if(x != min){
-		   aux2 = vetn[x];
-		   vetn[x] = vetn[min];
-		   vetn[min] = aux2;	
-		}
-	}
-		
-	for(x = 0; x < soma; x++){  //exibir o novo vetor    
-	  printf(" %i|",vetn[x]);
-	}	
+	}else if(soma == 1)
+	  vetn[x] = vetn[x];
+	else
+	  printf("Nao tem elementos para ordenar");  
 }
 
 
-void ordenaraux(no *vetor){
+void ordenaraux(no *vetor, int tamanho, int i){
 	//reutilizando de transforma()
-	int y = 0, x;
+	int y = 0, x = 0;//,min = 0, aux2;
+	no atual[TAM];
+	atual[i].vet2 = (aux *) malloc(sizeof(aux));
+	atual[i].vet2 = vetor[i].vet2;
 	
-	no novo[TAM];
+	int *nvet = (int*) malloc(tamanho * sizeof(int));
 	
-	for(x = 0; x < TAM; x++){
+	if(tamanho > 1){
+	
+			//passa pra vetor
+		    while(atual[i].vet2 != NULL){
+		    	nvet[y] =  atual[i].vet2->valor;
+		    	atual[i].vet2 = atual[i].vet2->prox;
+				y++;	
+			}
 		
-		novo[x].vet2 = (aux *) malloc(vetor[x].qnt * sizeof(aux)); 
-		novo[x].vet2 = vetor[x].vet2;  
+			//ordena
+			selectionsort(nvet, tamanho);
 		
-		int *tvet = (int*) malloc(vetor[x].qnt * sizeof(int));
-	
-	      while (novo[x].vet2 != NULL){
-	     	   tvet[y] = novo[x].vet2->valor;
-	           novo[x].vet2 = novo[x].vet2->prox;
-	           y++;
-	      }
-	     
-	for(x = 0; x < TAM; x++)
-		selectionsort(tvet,vetor[x].qnt); //chama selection para cada posicao 
-	}
-	    
+			//imprime
+	    	printf("\nVetor[%d] = \n",i+1);
+        	for(x = 0; x < tamanho; x++)
+				printf("   %d",nvet[x]);
+		
+ 	}else if (tamanho == 1)
+	  printf("\nVetor[%d] = %d\n",i+1, atual[i].vet2->valor);
+	else
+ 	  printf("\nVetor[%d] = vazio\n", i+1);
+ 	
+ 	//free(atual[tam].vet2);
+ 	//free(nvet);
+ 	
 }
 
 
-void ordenartodos(no *vetor){
-	int soma = 0, x = 0, y = 0, z;//variaveis criacao do vetor unico
+int ordenartodos(no *ini,int *tvet, int *nvet, int soma, int i, int y){
+	int z = 0, cont = 0;
 	
-	for(x = 0; x < TAM; x++){
-		if(vetor[x].qnt > 0)
-			soma += vetor[x].qnt;
+	if (soma > 0){
+				 
+	       while (ini[i].vet2 != NULL){
+	       			
+		           tvet[y] = ini[i].vet2->valor;
+		           nvet = tvet;
+//				   printf("     %d  ", nvet[y]);
+		           ini[i].vet2 = ini[i].vet2->prox;
+		           y++;
+			}
+			// y++;
+			
+			if(y >= soma){
+			    selectionsort(nvet, soma);			
+				while(z < soma){//para nao imprimir todas as vezes que entra na funcao
+					printf("  %d|",nvet[z++]);
+				}
+			}
+			return y;
 	}
-	
-	int *tvet = (int*) malloc(soma * sizeof(int));;// vetozao
-	
-	no unico[TAM]; 
-	
-	//criacao de um unico vetozao
-	for(x = 0; x < TAM; x++){ 
-	
-		unico[x].vet2 = (aux *) malloc(sizeof(aux)); //recebe estruturas aux
-		unico[x].vet2 = vetor[x].vet2;  
-		
-		//tvet = (int*) malloc(unico[x].qnt * sizeof(int)); //vetozao
-		
-		if(unico[x].qnt > 0){ //z para nao alterar y
-			y = unico[x].qnt; //a leitura das posicoes vai ser de tras pra frente. Ultima posicao, primeiro inserido
-		
-	       while (unico[x].vet2->prox != NULL){
-	           tvet[y] = unico[x].vet2->valor;
-	           unico[y].vet2 = unico[y].vet2->prox;
-	           y++;
-	       }
-	       
-    	} else
-    	  continue;
-    	  
-    }
-    
-    selectionsort(tvet, soma);
-
+				
 }
 
 
-int excluir(no *vetor){
-	int pos, exc, cont = 0;
-	
-	voltar: printf("\nPosicao principal para excluir: ");
-	scanf("%i", &pos);
-	
+int excluir(no *vetor, int pos, int exc){
+	int cont = 0;
+	no anterior[TAM];
+	no busca[1];
+	no elimine[TAM];
+		
 	if(tamanho(vetor, pos) == 0){
-		goto voltar;
+		return 0;
 	}
 	
-	//auxiliares de antes, durante e depois
-	no elimine[TAM], atual[TAM], anterior[TAM], posterior[TAM];
 	elimine[pos].vet2 = (aux *) malloc(sizeof(aux));
-	anterior[pos].vet2 = (aux *)malloc(sizeof(aux));
-	posterior[pos].vet2 = (aux *)malloc(sizeof(aux));
+	busca[pos].vet2 = (aux *) malloc(vetor[pos].qnt*sizeof(aux));
+	busca[pos].vet2 = vetor[pos].vet2; 
 	
-	printf("\Elemento a ser tirado: ");
-	scanf("%i", &exc);
+	if(busca[pos].qnt > 0){// se a lista nao estiver vazia
 	
-	atual[pos].vet2 = vetor[pos].vet2; 
+		//auxiliares de antes, durante e depois
+		anterior[pos].vet2 = (aux *)malloc(sizeof(aux));
+		anterior[pos].vet2 = NULL;
 	
-	if(atual[pos].qnt > 0){// se a lista nao estiver vazia
-	
-		while(atual[pos].vet2->prox != NULL){
-		
-			  if(atual[pos].vet2->valor == exc){
-			  	 elimine[pos].vet2 = atual[pos].vet2;
-			  	 
-				 break;		   
+		while(busca[pos].vet2 != NULL){
+			
+		     if(busca[pos].vet2->valor == exc){
+		     	
+		        if(busca[pos].vet2->valor == vetor[pos].vet2->valor){//se igual a inicio
+			  	   vetor[pos].vet2->prox = busca[pos].vet2->prox;
+			  	   elimine[pos].vet2 = busca[pos].vet2;
+			  	 }else
+			  	   anterior[pos].vet2->prox = busca[pos].vet2->prox; 
+				     
+				free(elimine[pos].vet2);
+				printf("Removido \n");
+				break;
+				
 		      } else{
 		      cont++;	
 			  }
-			
-	          atual[pos].vet2= atual[pos].vet2->prox;
+			  
+			  anterior[pos].vet2 = busca[pos].vet2;
+	          busca[pos].vet2= busca[pos].vet2->prox;
 		}
 
 
-    	if (cont = 0){
-	    	 liberar(elimine);
-	    	 printf("Removido \n");
-				 
-		} else{
-			printf("Elemento nao encontrado \n");
-		}  
+    	if (cont == busca[pos].qnt){
+	    	printf("Elemento nao encontrado \n");
+	    	return 0;
+		}
 		  
-		  
-	}else{
-	  printf("Não tem elementos alocados na estrutura\n");
-	  return 0;
 	}
-
-	liberar(atual);
+	
+	return 1;
 }
+	
